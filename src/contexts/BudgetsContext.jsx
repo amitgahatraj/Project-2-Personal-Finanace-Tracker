b08cef2a2e359a0174e3cb69d4d59d0b88a34383
Amit Gahatraj <amitgahatraj4@gmail.com>
2024-08-21 15:30:51 -0400

    Created useLocalStorage hook

--- src/contexts/BudgetsContext.jsx
+++ src/contexts/BudgetsContext.jsx
@@ -1,5 +1,6 @@
 import React, { createContext, useContext, useState } from "react";
 import { v4 as uuidV4 } from "uuid";
+import useLocalStorage from "../hooks/useLocalStorage";
 
 const BudgetsContext = createContext();
 
@@ -8,16 +9,35 @@ export function useBudgets() {
 }
 
 export const BudgetsProvider = ({ children }) => {
-  const [budgets, setBudgets] = [useState([])];
-  const [expenses, setExpense] = [useState([])];
+  const [budgets, setBudgets] = useLocalStorage("budgets", []);
+  const [expenses, setExpenses] = useLocalStorage("expenses", []);
 
   function getBudgetExpenses(budgetId) {
     return expenses.filter((expense) => expense.budgetId === budgetId);
   }
-  function addExpense() {}
-  function addBudget() {}
-  function deleteBudget() {}
-  function deleteExpense() {}
+  function addExpense({ description, amount, budgetId }) {
+    setExpenses((prevExpenses) => {
+      return [...prevExpenses, { id: uuidV4(), description, amount, budgetId }];
+    });
+  }
+  function addBudget({ name, max }) {
+    setBudgets((prevBudgets) => {
+      if (prevBudgets.find((budget) => budget.name === name)) {
+        return prevBudgets;
+      }
+      return [...prevBudgets, { id: uuidV4(), name, max }];
+    });
+  }
+  function deleteBudget({ id }) {
+    setBudgets((prevBudgets) => {
+      return prevBudgets.filter((budget) => budget.id !== id);
+    });
+  }
+  function deleteExpense({ id }) {
+    setExpenses((prevExpenses) => {
+      return prevExpenses.filter((expense) => expense.id !== id);
+    });
+  }
   return (
     <BudgetsContext.Provider
       value={{
